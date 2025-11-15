fc97ac776c171795457cc30f2d8620127a97e230
felipe-agustin <hola@felipeagustin.com>
2025-11-14 23:06:39 -0300

    colchon oscuro entre header y hero

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import './styles/main.css';
 import Header from './components/sections/Header/Header';
 import Hero from './components/sections/Hero/Hero';
 import ProblemSolution from './components/sections/ProblemSolution/ProblemSolution';
-import Results from './components/sections/Results/Results';
-// === 1. Importa el nuevo ContactForm ===
 import ContactForm from './components/sections/ContactForm/ContactForm.jsx';
 import Footer from './components/sections/Footer/Footer';
 import WhatsAppButton from './components/ui/WhatsAppButton/WhatsAppButton.jsx';
@@ -15,11 +13,13 @@ function App() {
   return (
     <>
       <Header />
+      
+      {/* --- ESTE ES EL NUEVO BLOQUE BLANCO --- */}
+      <div className="header-cushion-block"></div>
+      
       <main>
         <Hero />
         <ProblemSolution />
-        {/* <Results /> === 2. Añade el ContactForm aquí === */}
-        {/* === 2. Añade el ContactForm aquí === */}
         <ContactForm />
       </main>
       <Footer />
