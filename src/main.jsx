10216f5c52122c7f3e5e58f85d0081b64d63bc6a
felipe-agustin <hola@felipeagustin.com>
2025-11-15 22:42:57 -0300

    ajuste main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/main.css' // Importamos los estilos globales
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 1. Importar
+import './index.css';
+import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>  {/* 2. Envolver App con el Router */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+);
